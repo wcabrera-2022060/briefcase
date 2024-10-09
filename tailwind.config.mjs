de2f57117695ea65b8cfaf27ce7921d094160dba
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-10-09 13:13:37 -0600

    new version of the portfolio

---
+++ tailwind.config.mjs
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+    extend: {
+      keyframes: {
+        'infinite-scroll': {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        'infinite-scroll': 'infinite-scroll 10s linear infinite',
+      },
+      height: {
+        '22': '5.5rem',
+      }
+    },
+  },
+	plugins: [],
+}
